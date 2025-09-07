4a6495fb892c607bacc4be4ad043dc72991ba27d
GiriRaj249 <giriraj.ravichandran@freshworks.com>
2025-09-07 18:01:37 +0530

    Update Sample-fix.py

--- Sample-fix.py
+++ Sample-fix.py
@@ -4,3 +4,8 @@ def connect_api():
     api_key = os.getenv("API_KEY") 
     client = APIClient(api_key)
     return client
+
+def connect_api2():
+    api_key = os.getenv("test") 
+    client = APIClient(api_key)
+    return client
