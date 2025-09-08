1ca0b09cceb0ec60790cc9943c2d346247ab3542
GiriRaj249 <giriraj.ravichandran@freshworks.com>
2025-09-08 20:11:07 +0530

    Update Sample-fix.py

--- Sample-fix.py
+++ Sample-fix.py
@@ -4,7 +4,12 @@ def connect_api():
     api_key = os.getenv("API_KEY") 
     client = APIClient(api_key)
     return client
-
+    
+def name():
+    user_id = 42
+    id = cursor.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+    return id
+    
 def connect_api2():
     api_key = os.getenv("test") 
     client = APIClient(api_key)
